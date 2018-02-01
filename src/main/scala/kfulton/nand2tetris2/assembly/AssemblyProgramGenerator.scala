d7a719f7c673fb26ecdfd155f2d887d6ea72a874
Kate Fulton <kfulton@marchex.com>
2018-02-01 13:34:40 -0800

    Assembly clean up and refactoring

--- src/main/scala/kfulton/nand2tetris2/assembly/AssemblyProgramGenerator.scala
+++ src/main/scala/kfulton/nand2tetris2/assembly/AssemblyProgramGenerator.scala
@@ -1,75 +1,67 @@
 package kfulton.nand2tetris2.assembly
 
-sealed abstract class Register(val name: Option[String])
-case class VariableRegister(s: String) extends Register(None)
-case class NumberRegister(i: Int) extends Register(None)
-case object StackPointer extends Register(Some("@SP"))
-case object ArgumentPointer extends Register(Some("@ARG"))
-case object LocalPointer extends Register(Some("@LCL"))
-case object ThisPointer extends Register(Some("@THIS"))
-case object ThatPointer extends Register(Some("@THAT"))
-case object MRegister extends Register(Some("M"))
-case object DRegister extends Register(Some("D"))
-case object ARegister extends Register(Some("A"))
+case class Value(registerOrConstant: Either[RegisterExp, ConstantExp])
+
+sealed abstract class Register
+case class NameRegister(s: String) extends Register
+case class NumberRegister(i: Int) extends Register
+case class SpecialRegister(s: String) extends Register
 
 case class ConstantExp(i: Int)
 case class RegisterExp(r: Register)
 
 trait Expression
-case class EqualExpression(a: Either[RegisterExp, ConstantExp]) extends Expression
-case class AddExpression(a: Either[RegisterExp, ConstantExp], b: Either[RegisterExp, ConstantExp]) extends Expression
-case class SubtractExpression(a: Either[RegisterExp, ConstantExp], b: Either[RegisterExp, ConstantExp]) extends Expression
+case class AssignmentExpression(a: Value) extends Expression
+case class AddExpression(a: Value, b: Value) extends Expression
+case class SubtractExpression(a: Value, b: Value) extends Expression
 
 trait AssemblyCommand
-case class ValueAssignment(const: ConstantExp, expression: Expression) extends AssemblyCommand
 case class RegisterAssignment(reg: RegisterExp, expression: Expression) extends AssemblyCommand
-case class JumpNotEqual(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class JumpEqual(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class JumpGreaterThan(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class JumpGreaterThanOrEqual(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class JumpLessThan(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class JumpLessThanOrEqual(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
-case class Jump(a: Either[RegisterExp, ConstantExp]) extends AssemblyCommand
+case class JumpNotEqual(a: Value) extends AssemblyCommand
+case class JumpEqual(a: Value) extends AssemblyCommand
+case class JumpGreaterThan(a: Value) extends AssemblyCommand
+case class JumpGreaterThanOrEqual(a: Value) extends AssemblyCommand
+case class JumpLessThan(a: Value) extends AssemblyCommand
+case class JumpLessThanOrEqual(a: Value) extends AssemblyCommand
+case class Jump(a: Value) extends AssemblyCommand
 case class Loop(name: String) extends AssemblyCommand
 
 class AssemblyProgramGenerator {
 
   def generateAssemblyCode(command: AssemblyCommand) = command match {
     case jne: JumpNotEqual =>
-      val exp = getExp(jne.a); s"$exp; JNE"
+      val comparisonValue = getValue(jne.a); s"$comparisonValue; JNE"
     case jeq: JumpEqual =>
-      val exp = getExp(jeq.a); s"$exp; JEQ"
+      val comparisonValue = getValue(jeq.a); s"$comparisonValue; JEQ"
     case jgt: JumpGreaterThan =>
-      val exp = getExp(jgt.a); s"$exp; JGT"
+      val comparisonValue = getValue(jgt.a); s"$comparisonValue; JGT"
     case jge: JumpGreaterThanOrEqual =>
-      val exp = getExp(jge.a); s"$exp; JGE"
+      val comparisonValue = getValue(jge.a); s"$comparisonValue; JGE"
     case jlt: JumpLessThan =>
-      val exp = getExp(jlt.a); s"$exp; JLT"
+      val comparisonValue = getValue(jlt.a); s"$comparisonValue; JLT"
     case jle: JumpLessThanOrEqual =>
-      val exp = getExp(jle.a); s"$exp; JLE"
+      val comparisonValue = getValue(jle.a); s"$comparisonValue; JLE"
     case jmp: Jump =>
-      val exp = getExp(jmp.a); s"$exp; JMP"
-    case va: ValueAssignment =>
-      s"@${va.const.i} = ${getExpression(va.expression)}"
-    case ra: RegisterAssignment =>
-      val register = getRegister(ra.reg.r)
-      s"$register = ${getExpression(ra.expression)}"
+      val comparisonValue = getValue(jmp.a); s"$comparisonValue; JMP"
+    case assignment: RegisterAssignment =>
+      val register = getRegister(assignment.reg.r)
+      s"$register = ${getExpression(assignment.expression)}"
   }
 
-  def getRegister(r: Register) = r match {
-    case variable: VariableRegister => s"@${variable.s}"
+  def getRegister(r: Register): String = r match {
+    case name: NameRegister => s"@${name.s}" //TODO: Could I get rid of the @ and have it in name?
     case num: NumberRegister => s"@${num.i}"
-    case x: Register => x.name.get
+    case special: SpecialRegister => special.s
   }
 
-  private def getExp(a: Either[RegisterExp, ConstantExp]): String =  a match {
-    case Left(reg) => reg.r.name.getOrElse("")
+  private def getValue(a:Value): String =  a.registerOrConstant match {
+    case Left(reg) => getRegister(reg.r)
     case Right(const) => const.i.toString
   }
 
   def getExpression(exp: Expression): String = exp match {
-    case equal: EqualExpression => getExp(equal.a).toString
-    case add: AddExpression => s"${getExp(add.a)} + ${getExp(add.b)}"
-    case sub: SubtractExpression => s"${getExp(sub.a)} - ${getExp(sub.b)}"
+    case equal: AssignmentExpression => getValue(equal.a).toString
+    case add: AddExpression => s"${getValue(add.a)} + ${getValue(add.b)}"
+    case sub: SubtractExpression => s"${getValue(sub.a)} - ${getValue(sub.b)}"
   }
 }
