d392b560b8d8004221758cbf085b83bb6340905a
Kate Fulton <kfulton@marchex.com>
2018-03-01 14:48:28 -0800

    VmCommandMatcher logic

--- src/main/scala/kfulton/nand2tetris2/vmcommand/VMCommandMatcher.scala
+++ src/main/scala/kfulton/nand2tetris2/vmcommand/VMCommandMatcher.scala
@@ -1,9 +1,37 @@
 package kfulton.nand2tetris2.vmcommand
+import scala.util.matching.Regex.Match
+import scala.util.{Success, Try}
 
 class VMCommandMatcher {
-  val PopPattern = "\\s*(pop)\\s+(\\w+)\\s+(\\d+).*".r
-  val PushPattern = "\\s*(push)\\s+(\\w+)\\s+(\\d*).*".r
-  val AddPattern ="\\s+(add).*".r
+  val VMCapturePattern =  "^\\s*(\\w+)(?:\\s+(\\w+)\\s+(\\d+))?\\s*$".r
 
-  def generateVMCommand(command: String): VMCommand = ???
+  def generateVMCommand(command: String): Either[VMCommand, ParsingError] = {
+    val maybeRegexMatch = VMCapturePattern.findFirstMatchIn(command)
+    maybeRegexMatch match {
+      case Some(regexMatch) => findVMCommand(regexMatch)
+      case None => Right(ParsingError(s"Could not match command: $command."))
+    }
+  }
+
+  private def findVMCommand(result: Match) =
+    result.group(1).toLowerCase() match {
+      case "push" => createMemoryCommand(result, Push)
+      case "pop" => createMemoryCommand(result, Pop)
+      case s: String => VMCommand.fromString(s) match {
+        case Some(v) => Left(v)
+        case None => Right(ParsingError(s"Could not match: $result."))
+      }
+      case _ => Right(ParsingError(s"Could not match: $result."))
+    }
+
+  private def createMemoryCommand(result: Match, createFunction: (VMSegment, Int) => VMCommand) =
+    VMSegment.fromString(result.group(2)) match {
+      case Some(segment) =>
+        val locationTry = Try(result.group(3).toInt)
+        locationTry match {
+          case Success(i) => Left(createFunction(segment, i))
+          case _ => Right(ParsingError(s"Invalid location ${result.group(3)} for $result."))
+        }
+      case None => Right(ParsingError(s"Could not find segment: ${result.group(2)} for $result."))
+    }
 }
